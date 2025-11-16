00639b9f237a73b217088123b3fd114941b31cc1
Gonzalo Portilla <gonzalo.portilla@opendeusto.es>
2025-11-16 13:39:29 +0100

    Nuevo inicio Main + lib

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,25 @@
 package main;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 import gui.VentanaCarga; // 👈 importa tu clase VentanaCarga
 
 public class Main {
 
     public static void main(String[] args) {
-  
-        new VentanaCarga();
+    	System.out.println("Lanzando...");
+        
+    	try {
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println("ERROR: No se ha encontrado la librería de Look-and-Feel FlatLAF. Utilizando el LAF " +
+                    "por defecto.");
+        }
+        SwingUtilities.invokeLater(() -> new VentanaCarga());
     }
 
 }
