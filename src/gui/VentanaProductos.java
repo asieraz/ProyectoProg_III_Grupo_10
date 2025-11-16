aaf98c0f63e2d3cecd56707d6e6d20eeb495cd38
anderpalacin <ander.palacin@opendeusto.es>
2025-11-16 19:10:23 +0100

    c

--- src/gui/VentanaProductos.java
+++ src/gui/VentanaProductos.java
@@ -13,8 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableCellRenderer;
+
 
 import domain.Producto;
 
